49414aac2d796f1b45bc20926e83c989228413e0
koleganov <taggz1030@gmail.com>
2023-01-04 21:21:50 +0500

    'first'

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const todos = [
+    {
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+    },
+    {
+        title: 'Read next chapted of the book',
+        isCompleted: false,
+    },
+]
+
+const Home = () => {
+    return ( 
+        <div className="bg-gray-900 h-screen text-white">
+            {todos.map(todo => <div>{todo.title} </div> )}
+        </div>
+    )
+}
+
+export default Home
